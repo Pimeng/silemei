4733a8faef2b3a83efcba2ebcb2756507a6ac1e3
皮梦 <1470458485@qq.com>
2024-08-17 12:15:46 +0000

    修改读取的数据内容，更加合理。 修复代码错误，请更新这个版本，上一版本有问题。
    Signed-off-by: 皮梦 <1470458485@qq.com>

--- api.php
+++ api.php
@@ -1,61 +1,68 @@
 <?php
-
-//这个文件可作为api使用
-//链接后面跟上
-//?key=密钥&status=状态&goc=获取还是修改
-
 /*
 
 Made by Pimeng
 2024-08-03
-遵循Apache-2.0开源协议
+请遵循Apache-2.0开源协议
 
 */
 
+/*
+这个文件可作为api使用
+链接后面跟上
+?key=密钥&status=状态&goc=获取还是修改
+*/
+
+
+
+//开始创建数据库连接
+//定义用户名、密码和数据库
+$access = "密钥核对正确";
+$servername = "localhost";
+$username = "";
+$password = "";
+
 // 检查 URL 参数是否存在
-if(isset($_GET['key']) && isset($_GET['status']) && isset($_GET['goc'])){
+if(isset($_GET['key']) && isset($_GET['status']) && isset($_GET['goc']) && isset($_GET['name'])){
     $key = $_GET['key'];
     $status = $_GET['status'];
-    $goc = $_GET['goc']
+    $goc = $_GET['goc'];
+    $name = $_GET['name'];
 } else {
     echo "缺少参数";
+}
+
+//将传入参数给到变量
+$dbname = $name;
+// 创建连接
+$con = mysqli_connect($servername, $username, $password, "silemei_yedao");
+// 检测连接
+if (!$con) {
+    die("连接错误: " . mysqli_connect_error());
 }
 
 // 核对URL传入密钥
 if ($goc == 'change'){
     if($key == ''){//在这里定义你的密钥，可以是任意字符
-        //开始创建数据库连接
-        //定义用户名、密码和数据库
-        $access = "密钥核对正确";
-        $servername = "localhost";
-        $username = "";
-        $password = "";
-        $dbname = "data";
-        // 创建连接
-        $con = mysqli_connect($servername, $username, $password, $dbname);
-        // 检测连接
-        if (!$con) {
-            die("连接错误: " . mysqli_connect_error());
-        }
         // 写入数据库状态
         // 这里的代码还有优化的空间，但是我不会（
-        if ($status == '着了'){
-            //先删除原先存在的数据，防止数据过多
-            mysqli_query($con,"DELETE FROM web_data WHERE status='醒着'");
-            mysqli_query($con,"DELETE FROM web_data WHERE status='着了'");
-            mysqli_query($con,"DELETE FROM web_data WHERE status='未知'");
-            $sql = "INSERT INTO web_data (status) VALUES ('着了')";
+        if ($status == '0'){
+            //先删除原先存在的已知数据，防止数据过多
+            mysqli_query($con,"DELETE FROM $name WHERE status='0'");
+            mysqli_query($con,"DELETE FROM $name WHERE status='1'");
+            mysqli_query($con,"DELETE FROM $name WHERE status='2'");
+            $sql = "INSERT INTO $name (status) VALUES ('0')";
             if (mysqli_query($con, $sql)) {
-                echo "已切换至：着了";
+                echo "已切换至：睡似了";
             } else {
                 echo "Error: " . $sql . "<br>" . mysqli_error($con);
             }
-        } else if ($status == '醒着') {
+        } else if ($status == '1') {
             //先删除原先存在的数据，防止数据过多
-            mysqli_query($con,"DELETE FROM web_data WHERE status='着了'");
-            mysqli_query($con,"DELETE FROM web_data WHERE status='醒着'");
-            mysqli_query($con,"DELETE FROM web_data WHERE status='未知'");
-            $sql = "INSERT INTO web_data (status) VALUES ('醒着')";
+            mysqli_query($con,"DELETE FROM $name WHERE status='0'");
+            mysqli_query($con,"DELETE FROM $name WHERE status='1'");
+            mysqli_query($con,"DELETE FROM $name WHERE status='2'");
+            $sql = "INSERT INTO $name (status) VALUES ('醒着')";
             if (mysqli_query($con, $sql)) {
                 echo "已切换至：醒着";
             } else {
@@ -63,10 +70,10 @@ if ($goc == 'change'){
             }
         } else if ($status == '未知') {
             //先删除原先存在的数据，防止数据过多
-            mysqli_query($con,"DELETE FROM web_data WHERE status='着了'");
-            mysqli_query($con,"DELETE FROM web_data WHERE status='醒着'");
-            mysqli_query($con,"DELETE FROM web_data WHERE status='未知'");
-            $sql = "INSERT INTO web_data (status) VALUES ('未知')";
+            mysqli_query($con,"DELETE FROM $name WHERE status='着了'");
+            mysqli_query($con,"DELETE FROM $name WHERE status='醒着'");
+            mysqli_query($con,"DELETE FROM $name WHERE status='未知'");
+            $sql = "INSERT INTO $name (status) VALUES ('未知')";
             if (mysqli_query($con, $sql)) {
                 echo "已切换至：未知";
             } else {
@@ -82,7 +89,7 @@ if ($goc == 'change'){
 EOT;
 //密钥错误返回
 }} else if ( $goc == 'get'){//此处直接返回状态
-    $sql = "SELECT status FROM web_data"; 
+    $sql = "SELECT status FROM $name"; 
     $result = $con->query($sql);
 
     // 检查查询结果  
